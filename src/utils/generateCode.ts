ae687412e1fc1439aa1c75e010ce693c459f0bdb
Yutosaki <sasayu8190@gmail.com>
2024-08-06 11:52:44 +0900

    feat: 出力されるコードをモノホンに

--- src/utils/generateCode.ts
+++ src/utils/generateCode.ts
@@ -1,9 +1,22 @@
 export const generateCode = (name: string, items: string[]): string => {
-    const codeSnippets: { [key: string]: string } = {
-      stat: `const stats = { name: "${name}", value: 100 };`,
-      trophy: `const trophy = { name: "${name}", title: "Champion" };`,
-    };
-  
-    return items.map((item) => codeSnippets[item]).join('\n');
-  }
-  
+  const codeSnippets: { [key: string]: string } = {
+    stat: `
+      <h2 align="center">📚 GitHub Profile Summary Cards 📚</h2>
+      <br/>
+      <div align="center">
+          <img src="http://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=${name}&theme=tokyonight"/>
+          <img src="http://github-profile-summary-cards.vercel.app/api/cards/repos-per-language?username=${name}&theme=tokyonight"/>
+          <img src="http://github-profile-summary-cards.vercel.app/api/cards/most-commit-language?username=${name}&theme=tokyonight"/>
+          <img src="http://github-profile-summary-cards.vercel.app/api/cards/stats?username=${name}&theme=tokyonight"/>
+          <img src="http://github-profile-summary-cards.vercel.app/api/cards/productive-time?username=${name}&theme=tokyonight&utcOffset=9"/>
+      </div>`,
+    trophy: `
+      <h2 align="center">🏆 GitHub Profile Trophy 🏆</h2>
+      <br/>
+      <div align="center">
+          <img src="https://github-profile-trophy.vercel.app/?username=${name}&theme=tokyonight"/>
+      </div>`
+  };
+
+  return items.map((item) => codeSnippets[item]).join('\n');
+};
