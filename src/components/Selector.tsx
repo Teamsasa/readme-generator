132aa373e1c5381e0c24fd9f55b6d638d55b35ea
Yutosaki <sasayu8190@gmail.com>
2024-08-10 20:56:20 +0900

    feat: 全選択肢にcenter, right, leftを追加。また、削除ボタンをゴミ箱に

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -6,7 +6,7 @@ type CardData = {
   id: number;
   label1: string;
   label2: string;
-  label3?: string;
+  label3: string;
 };
 
 type SelectorProps = {
@@ -89,8 +89,7 @@ const Selector: React.FC<SelectorProps> = ({
       id: Date.now(),
       label1: options[0],
       label2: cardProfiles[0],
-      label3:
-        options[0] === "title" || options[0] === "body" ? alignProfiles[0] : "",
+      label3: alignProfiles[0],
     };
     setSelectedItems([...selectedItems, newCard]);
   };
@@ -110,10 +109,7 @@ const Selector: React.FC<SelectorProps> = ({
               ...card,
               [labelType]: value,
               label2: profiles[0],
-              label3:
-                value === "title" || value === "body"
-                  ? alignProfiles[0]
-                  : undefined,
+              label3: alignProfiles[0],
             };
           }
           return { ...card, [labelType]: value };
@@ -261,52 +257,56 @@ const DraggableCard: React.FC<DraggableCardProps> = ({
           ))}
         </select>
         {card.label1 === "title" || card.label1 === "body" ? (
-          <>
-            <input
-              type="text"
-              className="w-full p-2 border rounded bg-white dark:bg-neutral-800 dark:border-neutral-600"
-              value={card.label2}
-              onChange={(e) => updateLabel(card.id, "label2", e.target.value)}
-            />
-          </>
+          <input
+            type="text"
+            className="w-full p-2 border rounded mr-2 bg-white dark:bg-neutral-800 dark:border-neutral-600"
+            value={card.label2}
+            onChange={(e) => updateLabel(card.id, "label2", e.target.value)}
+            placeholder="テキストを入力"
+          />
         ) : (
           <select
-            className="w-full p-2 border rounded bg-white dark:bg-neutral-800 dark:border-neutral-600"
+            className="w-full p-2 border rounded mr-2 bg-white dark:bg-neutral-800 dark:border-neutral-600"
             value={card.label2}
             onChange={(e) => updateLabel(card.id, "label2", e.target.value)}
           >
-            {(card.label1 === "profile-trophy"
-              ? trophyProfiles
-              : cardProfiles
-            ).map((profile) => (
-              <option key={profile} value={profile}>
-                {profile}
-              </option>
-            ))}
+            {card.label1 === "profile-trophy"
+              ? trophyProfiles.map((profile) => (
+                  <option key={profile} value={profile}>
+                    {profile}
+                  </option>
+                ))
+              : cardProfiles.map((profile) => (
+                  <option key={profile} value={profile}>
+                    {profile}
+                  </option>
+                ))}
           </select>
         )}
-      </div>
-      {card.label1 === "title" || card.label1 === "body" ? (
-        <div className="flex justify-center mt-2">
-          <select
-            className="w-1/2 p-2 border rounded bg-white dark:bg-neutral-800 dark:border-neutral-600"
-            value={card.label3}
-            onChange={(e) => updateLabel(card.id, "label3", e.target.value)}
-          >
-            {alignProfiles.map((align) => (
-              <option key={align} value={align}>
-                {align}
-              </option>
-            ))}
-          </select>
-        </div>
-      ) : null}
-      <div className="flex justify-end mt-2">
+        <select
+          className="w-full p-2 border rounded bg-white dark:bg-neutral-800 dark:border-neutral-600"
+          value={card.label3}
+          onChange={(e) => updateLabel(card.id, "label3", e.target.value)}
+        >
+          {alignProfiles.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
         <button
           onClick={() => removeCard(card.id)}
-          className="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition duration-300"
+          className="ml-2 text-red-500 hover:text-red-700"
         >
-          削除
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M19 6h-4V5a3 3 0 0 0-6 0v1H5v2h14V6zm-7 0V5a1 1 0 1 1 2 0v1h-2zM6 9v12a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V9H6z" />
+          </svg>
         </button>
       </div>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,44 +8,45 @@ type CardData = {
   id: number;
   label1: string;
   label2: string;
-  label3?: string;
+  label3: string;
 };
 
 const Home: React.FC = () => {
   const [username, setUsername] = useState("");
-  const [selectedItems, setSelectedItems] = useState<CardData[]>([]);
+  const [selectedItems, setSelectedItems] = useState<CardData[]>([] as CardData[]);
   const [code, setCode] = useState("");
 
   const handleGenerateCode = () => {
-    const generatedCode = selectedItems
-      .map((item) => {
-        const label1Map: { [key: string]: string } = {
-          "Profile details card": "profile-details",
-          "Top languages used in repository card": "repos-per-language",
-          "Top languages in commits card": "most-commit-language",
-          "GitHub stats card": "stats",
-          "Productive time card": "productive-time",
-        };
+  const generatedCode = selectedItems
+    .map((item) => {
+      const label1Map: { [key: string]: string } = {
+        "Profile details card": "profile-details",
+        "Top languages used in repository card": "repos-per-language",
+        "Top languages in commits card": "most-commit-language",
+        "GitHub stats card": "stats",
+        "Productive time card": "productive-time",
+      };
 
-        if (item.label1 === "profile-trophy") {
-          return `<img src="https://github-profile-trophy.vercel.app/?username=${username}&rank=-?&theme=${item.label2}"/>`;
-        } else if (
-          Object.prototype.hasOwnProperty.call(label1Map, item.label1)
-        ) {
-          return `<img src="https://github-profile-summary-cards.vercel.app/api/cards/${
-            label1Map[item.label1]
-          }?username=${username}&count_private=true&theme=${item.label2}"/>`;
-        } else if (item.label1 === "title") {
-          return `<h1 align="${item.label3}">${item.label2}</h1>`;
-        } else if (item.label1 === "body") {
-          return `<p align="${item.label3}">${item.label2}</p>`;
-        } else {
-          return `Option: ${item.label1} could not be generated`;
-        }
-      })
-      .join("\n");
-    setCode(generatedCode);
-  };
+      if (item.label1 === "profile-trophy") {
+        return `<div align="${item.label3}"><img src="https://github-profile-trophy.vercel.app/?username=${username}&rank=-?&theme=${item.label2}"/></div>`;
+      } else if (
+        Object.prototype.hasOwnProperty.call(label1Map, item.label1)
+      ) {
+        return `<div align="${item.label3}"><img src="https://github-profile-summary-cards.vercel.app/api/cards/${
+          label1Map[item.label1]
+        }?username=${username}&count_private=true&theme=${item.label2}"/></div>`;
+      } else if (item.label1 === "title") {
+        return `<h1 align="${item.label3}">${item.label2}</h1>`;
+      } else if (item.label1 === "body") {
+        return `<p align="${item.label3}">${item.label2}</p>`;
+      } else {
+        return `Option: ${item.label1} could not be generated`;
+      }
+    })
+    .join("\n");
+  setCode(generatedCode);
+};
+
 
   return (
     <div className="container mx-auto py-10 p-4 bg-neutral-50 text-black dark:bg-neutral-800 dark:text-white">
