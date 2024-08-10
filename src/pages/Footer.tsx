8aa87c511f450024ba99f762f031ef8cc42366c0
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-08-10 19:30:31 +0900

    feat:LICENSE add to footer

---
+++ src/pages/Footer.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import LicenseModal from './LicenseModal';
+
+const Footer: React.FC = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return (
+    <footer className="bg-neutral-50 text-black dark:bg-neutral-800 dark:text-white py-4 text-center">
+      <p>
+        <button onClick={openModal}>Software product license</button>
+      </p>
+      {isModalOpen && <LicenseModal onClose={closeModal} />}
+    </footer>
+  );
+};
+
+export default Footer;
