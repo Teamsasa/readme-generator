af778291508fa3dd1ae20c2a795c4f010fb5e32b
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-08-11 01:21:53 +0900

    fix:text

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ const App: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <header>
-        <h1 className="text-4xl font-bold text-center my-8">
-          Readme Generator
+        <h1 className="text-5xl font-bold text-center my-8">
+          Readme Studio
         </h1>
       </header>
       <main className="flex-grow p-4">
