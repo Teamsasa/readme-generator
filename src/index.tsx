c26902f1c8f76f52d7585b5245af2c85864592a7
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-08-11 13:47:54 +0900

    fix:html head

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 );
