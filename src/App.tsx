f815ef2aa0ec54e413058f40a556771d9887b202
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-08-11 11:04:50 +0900

    feat:html header

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import React from "react";
 import Home from "./pages/Home";
 import Footer from "./pages/Footer";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 const App: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-cover bg-center dark:bg-[url('/bg-img-dark.jpg')]">
-      <header>
-        <h1 className="text-5xl font-bold text-center my-8">Readme Studio</h1>
-      </header>
-      <main className="flex-grow p-4">
-        <Home />
-      </main>
-      <Footer />
-    </div>
+    <HelmetProvider>
+      <div className="flex flex-col min-h-screen bg-cover bg-center dark:bg-[url('/bg-img-dark.jpg')]">
+        <Helmet>
+          <title>Readme Studio</title>
+          <meta
+            name="description"
+            content="A simple and customizable GitHub Profile generator."
+          />
+          <meta property="og:title" content="Readme Studio" />
+          <meta
+            property="og:description"
+            content="A simple and customizable GitHub Profile generator."
+          />
+          <meta property="og:image" content="../public/og-img.jpg" />
+          <link rel="icon" href="../public/og-img.jpg" />
+        </Helmet>
+        <header>
+          <h1 className="text-5xl font-bold text-center my-8">Readme Studio</h1>
+        </header>
+        <main className="flex-grow p-4">
+          <Home />
+        </main>
+        <Footer />
+      </div>
+    </HelmetProvider>
   );
 };
+
 export default App;
