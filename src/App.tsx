8d90e58e069dc3043d820921b42f8b272556ea33
yamamoto99 <yamamoto1952hkb@gmail.com>
2024-08-11 13:50:06 +0900

    fix:move helmt to index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
 import React from "react";
 import Home from "./pages/Home";
 import Footer from "./pages/Footer";
-import { Helmet } from "react-helmet-async";
 
 const App: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-cover bg-center dark:bg-[url('/bg-img-dark.jpg')]">
-      <Helmet>
-        <title>Readme Studio</title>
-        <meta
-          name="description"
-          content="A simple and customizable GitHub Profile generator."
-        />
-        <meta property="og:title" content="Readme Studio" />
-        <meta
-          property="og:description"
-          content="A simple and customizable GitHub Profile generator."
-        />
-        <meta property="og:image" content="/og-img.jpg" />
-        <link rel="icon" href="/og-img.jpg" />
-      </Helmet>
       <header>
         <h1 className="text-5xl font-bold text-center my-8">Readme Studio</h1>
       </header>
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
-import { HelmetProvider } from "react-helmet-async";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
+      <Helmet>
+        <title>Readme Studio</title>
+        <meta
+          name="description"
+          content="A simple and customizable GitHub Profile generator."
+        />
+        <meta property="og:title" content="Readme Studio" />
+        <meta
+          property="og:description"
+          content="A simple and customizable GitHub Profile generator."
+        />
+        <meta property="og:image" content="/og-img.jpg" />
+        <link rel="icon" href="/og-img.jpg" />
+      </Helmet>
       <App />
     </HelmetProvider>
   </React.StrictMode>,
